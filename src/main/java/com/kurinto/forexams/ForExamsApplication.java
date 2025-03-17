11343d98fd6680f55a2cd8d757d8c28e1a95240b
kurinto <barbosaclint@gmail.com>
2025-03-17 12:36:13 +0800

    initial commit

---
+++ src/main/java/com/kurinto/forexams/ForExamsApplication.java
@@ -0,0 +1,13 @@
+package com.kurinto.forexams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForExamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForExamsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kurinto/forexams/ForExamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurinto.forexams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForExamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
